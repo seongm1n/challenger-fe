2329fc75e09fbd56d43887fe2856e7d79b5bb658
seongm1n <0411tjdals34@gmail.com>
2025-04-08 15:30:10 +0900

    initial commit

---
+++ challengerTests/challengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  challengerTests.swift
+//  challengerTests
+//
+//  Created by theo on 4/8/25.
+//
+
+import Testing
+@testable import challenger
+
+struct challengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
